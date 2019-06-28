940247c75dd069d3961ed7e8a981f1a157dd744c
altieresl <altieresl@outlook.com>
2019-06-28 00:36:33 -0300

    Código comentado

--- trabalho.py
+++ trabalho.py
@@ -4,11 +4,10 @@ class UH:
 	def __init__(self, entrada):
 		aux = entrada.split("000")
 		maquina = aux[1]
-		palavra = aux[2]
+		palavra = aux[2] #W
 		self.fita1 = maquina.split("00") #Configuração da fita
 		self.fita2 = "1" #Estado atual
 		self.fita3 = palavra.split("0") #Palavra
-
 		self.arrSnapshots = []
 
 	def executar(self):
@@ -23,10 +22,9 @@ class UH:
 				simboloAtual = self.fita3[indiceSimbolo]
 				estadoAtual = self.fita2
 
-				#Euristica do snapshot
+				#Euristica do snapshot - explicação dentro das funções
 				encontrouLoop = self.buscarSnapshot(estadoAtual, indiceSimbolo, self.fita3)
 				self.adicionarSnapshot(estadoAtual, indiceSimbolo, self.fita3)
-
 
 				encontradaTransicao = False
 				for i in range(len(self.fita1)):
@@ -36,15 +34,21 @@ class UH:
 						self.fita3[indiceSimbolo] = transicao.escrita
 						self.fita2 = transicao.proximoEstado
 						indiceSimbolo = ( indiceSimbolo+1 if transicao.direcao == "1" else indiceSimbolo - 1)
-
-						if(indiceSimbolo > len(self.fita3)-1):
+						if(indiceSimbolo > len(self.fita3)-1 and not encontrouLoop):
+							'''
+							Aproveitando da ideia de se acrescentar um B (111) no final da fita quando o indice do símbolo a ser lido é maior que o tamanho da fita,
+							antes de acrescentar um BBB podemos verificar se a transição é do tipo "Lê B, continua no mesmo estado e vai pra direita", caso seja, sabemos que temos um loop (é uma heurística)
+							'''
 							encontrouLoop = self.verificarLoopBFimFita(transicao)
 
 				if(indiceSimbolo > len(self.fita3)-1): #Se o índice do símbolo (posição de um 'caractere' da palavra de entrada) for maior que a quantidade de símbolos da entrada nós adicionamos um B no final da fita, simulando uma fita com B infinitos
 					self.fita3.append("111")
-			if(encontrouLoop):
-				print("A máquina entrou em loop")
-	def verificarSeEhFinita(self):
+		if(not encontrouLoop or maquinaEhFinita):
+			print("A máquina termina a execução corretamente")
+		else:
+			print("A máquina entrou em loop")
+
+	def verificarSeEhFinita(self): #Heurística - Ao verificar que a máquina não tem loop de leitura de B, não leitura de B após o símbolo inicial e não tem transição para a esquerda, sabemos que ela é finita e não precisamos analisar
 		fita = self.fita1
 		semBAlemEstadoInicial = True
 		semTransicaoParaEsquerda = True
@@ -66,17 +70,22 @@ class UH:
 		return (transicao.leitura == "111" and transicao.estadoAtual == transicao.proximoEstado and transicao.direcao == "1")
 
 
-	def adicionarSnapshot(self, estadoAtual, indiceSimbolo, fita3):
+	def adicionarSnapshot(self, estadoAtual, indiceSimbolo, fita3): #Um snapshot é uma tupla que indica o estado atual, o índice da cabeça de leitura e uma cópia do estado atual da fita
 		self.arrSnapshots.append((estadoAtual, indiceSimbolo, copy.copy(fita3)))
 
-	def buscarSnapshot(self, estadoAtual, indiceSimbolo, fita3):
+	def buscarSnapshot(self, estadoAtual, indiceSimbolo, fita3): 
+		'''
+		Quando encontramos um snapshot armazenado igual ao snapshot passado por parâmetro (da leitura atual),
+	 	isso significa que a máquina de turing já passou por aquela transição, lendo exatamente aquele símbolo (garantido ao se comparar o índice do simbolo e não o símbolo e si) e a fita não se modificou,
+	 	logo sabemos que a máquina entrou em loop
+	 	'''
 		for snapshot in self.arrSnapshots:
 			if(snapshot == (estadoAtual, indiceSimbolo, fita3)):
 				return True
 		return False
 			
 				
-class Transicao:
+class Transicao: #Classe auxiliar para transformar uma string de transição em um objeto, visando deixar o código mais claro
 	def __init__(self, stringTransicao):
 		aux = stringTransicao.split("0")
 		self.estadoAtual = aux[0]
@@ -86,31 +95,12 @@ class Transicao:
 		self.direcao = aux[4]
 
 def main():
-	'''
-	000 = começo
-	000 = fim
-	R = 1
-	L = 11
-	primeira parte da fita = estado atual
-	segunda 			=    simbolo atual
-	terceira			=    proximo estado
-	quarta 				=    escrita
-	quinta				=    direção
-	'''
-
-	entrada2 = "0001011101101110100110101110101001110111011011101000111010111"
-	entrada2 = "000101110110111010011010111010100110110111101101001110110111011010011101110111110111011001111010111101010011110111011111011101100011101011011011000"
-	# entrada2 = "000"+"1011101101110100"+"11010111010100"+"111011011011011"+"000"+"11101011"
-	# entrada2 = "000"+"1011101101110100"+"11010111010100"+"11101101111011101"+"000"+"11101"
-	entrada2 = "000"+"1011101011101"+"000"+"111"
-
 	entrada = ""
 	if(len(sys.argv) > 1):
 		entrada = sys.argv[1]
 	else:
 		print("É necessário passar um argumento com a configuração da máquina e a palavra rm(w)")
-	#Entrada = 11101011011011000
-	objUH = UH(entrada2)
+	objUH = UH(entrada)
 	objUH.executar()
 
 if __name__ == '__main__':
